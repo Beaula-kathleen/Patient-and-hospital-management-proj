77c4260c9536987c7647818c9b57b9e16363039e
Beaula-kathleen <92220656+Beaula-kathleen@users.noreply.github.com>
2024-12-19 12:02:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginAndSignin from './pages/LoginAndSignin';
+
+function App() {
+  return (
+    <div className="App">
+    <LoginAndSignin/>
+    </div>
+  );
+}
+
+export default App;
