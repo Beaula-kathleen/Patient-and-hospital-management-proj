f47e1b093960d4cd3055595bc9060440188ef9a8
gayathri <gaya306vijay@gmail.com>
2024-12-20 12:12:36 +0530

    creation of specialist module

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <div>Home
+     <Link to="/testing">← Go to demo</Link>
+  </div>;
 }
 
-export default Home
+export default Home;
---
+++ src/pages/Specialist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Specialist() {
+
+    console.log("Specialist page loaded");
+    
+  return (
+    <div>aewt4erw</div>
+  )
+}
+
+export default Specialist
